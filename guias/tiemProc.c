f6f6cbac68ec822d702ef3fcd1b16006e520e808
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2023-10-01 17:37:07 -0300

    ejercicio semiperimetro

--- guias/tiemProc.c
+++ guias/tiemProc.c
@@ -1,6 +1,33 @@
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <time.h>
+void main()
+{
+    pid_t pid_padre, pid_hijos, proc1, proc2;
+    clock_t inicio, fin;
+    double tiempo_cpu;
+    pid_padre = fork();
 
-int main() {
-    printf("hola mundo");
+    if (pid_padre == 0)
+    {
+        for (int i = 1; i <= 50; i++)
+        {
+            inicio = clock();
+            proc1 = getppid();
+            proc2 = getpid();
+            fin = clock() - inicio;
+
+            tiempo_cpu = ((double) fin) / CLOCKS_PER_SEC;
+
+            printf("Soy el hijo %d, Mi padre es= %d, Mi PID= %d\n", i, proc1, proc2);
+            printf("Tiempo: %f segundos\n", tiempo_cpu);
+        }
+        printf("Proceso PADRE = %d\n", getppid());
+    }
+    else
+    {
+        pid_hijos = wait();
+    }
     return 0;
 }
--- guias/triSemi.c
+++ guias/triSemi.c
@@ -3,7 +3,7 @@
 
 int main() {
     int a, b, c;
-    int semi, area;
+    float semi, area;
     printf("Ingrese un lado del triangulo: ");
     scanf("%d",&a);
     printf("Ingrese un lado del triangulo: ");
@@ -11,10 +11,10 @@ int main() {
     printf("Ingrese un lado del triangulo: ");
     scanf("%d",&c);
     if (a + b > c && a + c > b && b + c > a) {
-        semi = (a + b + c) / 2;
+        semi = (a + b + c) / 2.0;
         area = sqrt(semi * (semi - a) * (semi - b) * (semi - c));
-        printf("El semiperimetro es: %d\n",semi);
-        printf("El area es: %d\n",area);
+        printf("El semiperimetro es: %f\n",semi);
+        printf("El area es: %f\n",area);
     }
     else {
         printf("Triangulo no valido\n");
